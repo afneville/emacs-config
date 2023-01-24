eb2bdf89a2631e320e9b572aab3d9c462645c86f
Alexander Neville <git@afneville.com>
2023-01-24 23:34:44 +0000

    move to literate config

--- custom_vars.el
+++ custom_vars.el
@@ -1,16 +1,13 @@
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(which-key projectile visual-fill-column use-package spacemacs-theme org-bullets org-appear ivy-rich general evil-collection doom-themes doom-modeline dashboard counsel base16-theme all-the-icons)))
+   '(yasnippet-snippets yasnippet smartparens company lsp-pyright lsp-mode tree-sitter-langs tree-sitter which-key general evil-collection evil counsel ivy-rich ivy doom-modeline all-the-icons doom-themes use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Iosevka Nerd Font" :height 90))))
- '(fixed-pitch ((t (:family "Iosevka Nerd Font" :height 90))))
- '(variable-pitch ((t (:family "ETBookOT" :height 110)))))
+ )
