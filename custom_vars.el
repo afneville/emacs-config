82d8810403b732ab7489c1b822df30ac26fc4930
Alexander Neville <git@afneville.com>
2023-02-28 21:23:32 +0000

    support for emacs --daemon

--- custom_vars.el
+++ custom_vars.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-echo-area-message "alex")
- '(org-agenda-files '("~/tmp/test.org"))
+ '(org-agenda-files nil)
  '(package-selected-packages
-   '(company-box org-fragtog auctex cdlatex org-modern mixed-pitch doom-modeline all-the-icons visual-fill-column org-appear org-bullets yasnippet-snippets yasnippet smartparens company lsp-pyright lsp-mode tree-sitter-langs tree-sitter which-key general evil-collection evil counsel ivy-rich ivy modus-themes use-package)))
+   '(projectile company-box org-fragtog auctex cdlatex org-modern mixed-pitch doom-modeline all-the-icons visual-fill-column org-appear org-bullets yasnippet-snippets yasnippet smartparens company lsp-pyright lsp-mode tree-sitter-langs tree-sitter which-key general evil-collection evil counsel ivy-rich ivy modus-themes use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
